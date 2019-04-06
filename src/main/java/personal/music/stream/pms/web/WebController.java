2f17059665b10584e499f9887db85b9de46b3d3a
Theo Moorman <theo.moorman@sogeti.com>
2019-04-06 16:20:01 +0200

    Player endpoint to start one song.

--- src/main/java/personal/music/stream/pms/web/WebController.java
+++ src/main/java/personal/music/stream/pms/web/WebController.java
@@ -1,13 +1,23 @@
 package personal.music.stream.pms.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
 
-	@RequestMapping("/")
+	@RequestMapping("/home")
 	public String home() {
 		return "index";
 	}
+
+	@GetMapping("/player/{mix_id}")
+	public String homeWithParam(@PathVariable String mix_id, Model model) {
+		model.addAttribute("mix_id", mix_id);
+		return "index";
+	}
+
 }
