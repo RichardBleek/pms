f654bfa865910f0328fdf151089a292bd0c81ac2
Theo Moorman <theo.moorman@sogeti.com>
2019-04-06 16:38:55 +0200

    Player mapping only.

--- src/main/java/personal/music/stream/pms/web/WebController.java
+++ src/main/java/personal/music/stream/pms/web/WebController.java
@@ -4,18 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
 
-	@RequestMapping("/home")
-	public String home() {
-		return "index";
-	}
-
 	@GetMapping("/player/{mix_id}")
-	public String homeWithParam(@PathVariable String mix_id, Model model) {
+	public String player(@PathVariable String mix_id, Model model) {
 		model.addAttribute("mix_id", mix_id);
 		return "index";
 	}
